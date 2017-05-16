525320e7eec7e891914810a0908d6b4b3d9fd0e9
benrau87 <benrau87@users.noreply.github.com>
2017-05-16 15:53:17 -0500

    Update start_cuckoo.sh

--- supporting_scripts/start_cuckoo.sh
+++ supporting_scripts/start_cuckoo.sh
@@ -3,15 +3,15 @@ RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-ON=$(ifconfig -a | grep -cs 'vboxnet0')
+ON=$(ifconfig -a | grep -cs 'vvoxnet0')
 
 if [[ $ON == 1 ]]
 then
   echo "Host only interface is up"
-  VBoxManage hostonlyif ipconfig vboxnet0 --ip 10.1.1.254
+  VBoxManage hostonlyif ipconfig vvoxnet0 --ip 10.1.1.254
 else
-  VBoxManage hostonlyif create vboxnet0
-  VBoxManage hostonlyif ipconfig vboxnet0 --ip 10.1.1.254
+  VBoxManage hostonlyif create vvoxnet0
+  VBoxManage hostonlyif ipconfig vvoxnet0 --ip 10.1.1.254
 fi
 
 xterm -hold -e cuckoo &
