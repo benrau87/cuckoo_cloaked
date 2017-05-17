e5b40c161f856e1fe193d783e6e09a3fb2e0a3bb
benrau87 <benrau87@users.noreply.github.com>
2017-05-17 11:10:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -117,6 +117,7 @@ cp $gitdir/supporting_scripts/start_cuckoo.sh /home/$name/
 cd tools/
 
 ###Add Repos
+install_packages python-software-properties software-properties-common
 ##Mongodb
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 &>> $logfile
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list &>> $logfile
