d86f0d0d4b411984b686c7397deafb321cc968dd
benrau87 <benrau87@users.noreply.github.com>
2017-05-18 14:52:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -192,8 +192,7 @@ INNOTEK=XNNOTEK
 PCI80EE=80EF
 PCI80ee=80ef
 
-touch virtualboxlogfile.log
-vlogfile = "$PWD/virtualboxlogfile.log"
+vlogfile="$(basename -s $logfile $0).out"
 me="$(basename $0)"
 count=0
 # Rename files and folders arg1=string in filename to search for, arg2=string to rename filename to
