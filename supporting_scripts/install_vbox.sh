972f9a5695c80bd3f8e4f0527477a5cb6fb33a78
benrau87 <benrau87@users.noreply.github.com>
2017-05-30 10:59:20 -0500

    Update install_vbox.sh

--- supporting_scripts/install_vbox.sh
+++ supporting_scripts/install_vbox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp/
-tee -a "deb-src http://us.archive.ubuntu.com/ubuntu/ xenial-backports" /etc/apt/sources.list
+tee -a  /etc/apt/sources.list "deb-src http://us.archive.ubuntu.com/ubuntu/ xenial-backports"
 dpkg --add-architecture i386
 apt-get update
 apt-get -y install gcc g++ bcc iasl xsltproc uuid-dev zlib1g-dev libidl-dev \
