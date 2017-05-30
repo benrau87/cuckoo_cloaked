b1f7c749e476a3faffebd7689495d28e87fcd1f6
benrau87 <benrau87@users.noreply.github.com>
2017-05-30 14:55:07 -0500

    Update install_vbox.sh

--- supporting_scripts/install_vbox.sh
+++ supporting_scripts/install_vbox.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-
+apt-get install subversion build-essential bcc iasl xsltproc uuid-dev \
+    zlib1g-dev libidl-dev libsdl1.2-dev libxcursor-dev libasound2-dev \
+    libstdc++5 libpulse-dev libxml2-dev libxslt1-dev pyqt5-dev-tools \
+    libqt5opengl5-dev qtbase5-dev-tools libcap-dev libxmu-dev \
+    mesa-common-dev libglu1-mesa-dev linux-libc-dev libcurl4-openssl-dev \
+    libpam0g-dev libxrandr-dev libxinerama-dev libqt5opengl5-dev makeself \
+    libdevmapper-dev default-jdk texlive-latex-base texlive-latex-extra \
+    texlive-latex-recommended texlive-fonts-extra texlive-fonts-recommended \
+    lib32ncurses5 lib32z1 libc6-dev-i386 lib32gcc1 gcc-multilib \
+    lib32stdc++6 g++-multilib genisoimage libvpx-dev qt5-default \
+    qttools5-dev-tools libqt5x11extras5-dev libssl-dev python-all-dev \
+    git-svn kbuild iasl libpng-dev libsdl-dev yasm
+    
 cd /home/cuckoo/sources/
-svn co http://www.virtualbox.org/svn/vbox vbox
+svn co http://www.virtualbox.org/svn/vbox/trunk vbox
 # Directories and filenames
-SOURCESDIR=/home/cuckoo/sources/vbox/trunk                 # Dir where the vbox source code is
+SOURCESDIR=/home/cuckoo/sources/vbox                 # Dir where the vbox source code is
 KMKTOOLSSUBDIR=kBuild/bin/linux.amd64                   # where we find the kmk tools e.g. kmk_md5sum
 MD5SUMOUT=$SOURCESDIR/kmk_md5.out                   # log md5sum ops to this file
 VBOXMANAGE=$SOURCESDIR/out/linux.amd64/release/bin/VXoxManage       # location and name of VBoxManage binary (after renamed)
